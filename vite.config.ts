36aea52fe692fc9acc172645149d3b069a60f726
zhaozejun <zhaozejun@joyuai.com>
2024-09-18 11:48:55 +0800

    remove vite-plugin-vue-devtools

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { defineConfig } from 'vite'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +12,6 @@ export default defineConfig({
     plugins: [
         vue(),
         vueJsx(),
-        vueDevTools(),
     ],
     resolve: {
         alias: {
